1ccaaf12904b6256e53a7938a08b9eaf30e1ee00
iamswart <swartjide@gmail.com>
2020-05-12 23:04:16 +0100

    adding file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include "db.php"; ?>
+<?php include "header.php"; ?>
+
+<?php include "navigation.php"; ?>
+
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div id="content">
+                        <h1>Lit Locker</h1>
+                        <h3>Africa's No 1 online store</h3>
+                        <hr class="line">
+                        <button class="btn btn-outline-secondary btn-lg"><i class="fa fa-paw"></i><a href="store.php"> Explore!</a></button>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+
+<?php include "footer.php" ?>
+        
